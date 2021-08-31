2a8086f244c213c530de58354e7be2f90862f184
Leon2ev <jevgeni.glazunov@gmail.com>
2021-08-31 20:40:39 +0200

    Signal confiramtion on 4h time frame

--- signals/technicals.py
+++ signals/technicals.py
@@ -16,45 +16,48 @@ class AwesomeOscillator():
         self.symbols = symbols
         self.client = client
 
+    async def get_ao(self, symbol: str, interval: str) -> None:
+
+        '''Get Awesome Oscillator(AO) values for the last two closed candles
+        on 1h time frame(TF) in this case indexes 33 and 34. If the value of 
+        33 is less than 0 and the value of 34 is greater than 0 then we confirm 
+        signal on 4h TF. If the last closed candle on 4h has AO value greater
+        than 0 then signal is sent'''
+
+        candles = await self.client.get_klines(
+            symbol=symbol,
+            interval=interval,
+            limit=36)
+
+        candles_df = df(candles, columns=[
+            'Open Time',
+            'Open',
+            'High',
+            'Low',
+            'Close',
+            'Volume',
+            'Close Time',
+            'Quote Volume',
+            'Trades',
+            'Taker buy base',
+            'Taker buy quote',
+            'Ignore'])
+
+        high = pd.to_numeric(candles_df['High'])
+        low = pd.to_numeric(candles_df['Low'])
+        indicator = AwesomeOscillatorIndicator(high, low)
+        ao_values = indicator.awesome_oscillator()
+        if ao_values[34] > 0 and ao_values[33] < 0:
+            if interval == '1h':
+                interval = str(self.client.KLINE_INTERVAL_4HOUR)
+                await self.get_ao(symbol, interval)
+            else:
+                print(symbol, 'bingo')
+
     async def run(self) -> None:
-
-        '''Loop through the symbols and fetch candles every hour.
-        Get Awesome Oscillator values for the last two closed candles
-        in this case indexes 33 and 34. If the value of 33 is less
-        than 0 and the value of 34 is greater than 0 signal is sent.'''
-
+        interval = str(self.client.KLINE_INTERVAL_1HOUR)
         while True:
             for symbol in self.symbols:
-                candles = await self.client.get_klines(
-                    symbol=symbol,
-                    interval=self.client.KLINE_INTERVAL_1HOUR,
-                    limit=36
-                )
-
-                candles_df = df(candles, columns=[
-                'Open Time',
-                'Open',
-                'High',
-                'Low',
-                'Close',
-                'Volume',
-                'Close Time',
-                'Quote Volume',
-                'Trades',
-                'Taker buy base',
-                'Taker buy quote',
-                'Ignore'
-                ])
-
-                high = pd.to_numeric(candles_df['High'])
-                low = pd.to_numeric(candles_df['Low'])
-                indicator = AwesomeOscillatorIndicator(high, low)
-                ao_values = indicator.awesome_oscillator()
-                if ao_values[34] > 0 and ao_values[33] < 0:
-                    print(symbol, 'bingo')
+                await self.get_ao(symbol, interval)
 
             time.sleep(60 * (60 - datetime.now().minute))
-
-    
-    
-    
